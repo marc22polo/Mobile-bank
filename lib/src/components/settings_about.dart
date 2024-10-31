df0d0c89a4660a99f839594f54cd1e432dc7fff1
Marko Žele <zele.marko5@gmail.com>
2024-11-01 00:30:33 +0100

    settings ui rework

---
+++ lib/src/components/settings_about.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SettingsAbout {
+  const SettingsAbout();
+
+  static void showTransactionDetail(BuildContext context) {
+    double mHeight = MediaQuery.of(context).size.height;
+
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: mHeight,
+          padding: const EdgeInsets.all(15),
+          color: Theme.of(context).colorScheme.surface,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "About",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Row(
+                  children: [
+                    Text("Version:"),
+                    Text("0.0.1 alpha"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/src/page/settings.dart
+++ lib/src/page/settings.dart
@@ -1,5 +1,8 @@
+import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/components/settings_about.dart';
 import 'package:flutter_application_1/src/theme/theme_provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -17,26 +20,100 @@ class _SettingsPageState extends State<SettingsPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          children: [
-            const Text("Dark mode"),
-            Checkbox(
-              value: darkMode,
-              onChanged: (value) {
-                Provider.of<ThemeProvider>(context, listen: false)
-                    .toggleTheme();
-                setState(() {
-                  darkMode = Provider.of<ThemeProvider>(context, listen: false)
-                      .isDarkMode;
-                });
-              },
-            ),
-          ],
+        iconTheme: IconThemeData(
+          color: Theme.of(context).colorScheme.onPrimary,
         ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          AppLocalizations.of(context)!.settingsTitle,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+            fontSize: 18,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20, left: 16),
+              child: Text(
+                "Personalization",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: SettingsGroup(
+              backgroundColor: Theme.of(context).colorScheme.tertiary,
+              items: [
+                SettingsItem(
+                  icons: Icons.dark_mode_outlined,
+                  title: "Dark mode",
+                  titleStyle: const TextStyle(
+                    fontWeight: FontWeight.normal,
+                  ),
+                  trailing: Switch.adaptive(
+                    value: darkMode,
+                    onChanged: (value) {
+                      Provider.of<ThemeProvider>(context, listen: false)
+                          .toggleTheme();
+                      setState(() {
+                        darkMode =
+                            Provider.of<ThemeProvider>(context, listen: false)
+                                .isDarkMode;
+                      });
+                    },
+                  ),
+                ),
+                SettingsItem(
+                  icons: Icons.language_outlined,
+                  title: "Language (WIP)",
+                  titleStyle: const TextStyle(
+                    fontWeight: FontWeight.normal,
+                  ),
+                  subtitle: "English",
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 5, left: 16),
+              child: Text(
+                "About",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: SettingsGroup(
+              backgroundColor: Theme.of(context).colorScheme.tertiary,
+              items: [
+                SettingsItem(
+                  icons: Icons.info_outline,
+                  title: "About",
+                  titleStyle: const TextStyle(
+                    fontWeight: FontWeight.normal,
+                  ),
+                  onTap: () => SettingsAbout.showTransactionDetail(context),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
